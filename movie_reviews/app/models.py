36a13b58d7c27ec5a43d203823d3079807966a46
gianniskokkosis <t8160190@aueb.gr>
2020-08-16 19:04:25 +0300

    Create models Movie and Review
    - Create database table Movie that will hold all the necessary informations about movies.
    - Create database table Review that will hold all the reviews for different movies.

--- movie_reviews/app/models.py
+++ movie_reviews/app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    director = models.CharField(max_length=100)
+    cast = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+    
+
+class Review(models.Model):
+    review_title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_added = models.DateField(auto_now_add=True)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.review_title
